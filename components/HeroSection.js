43a79a428523d0c3536125b2f3966e387384bc93
Adarsh Dubey <adarshdubeyisro03@gmail.com>
2025-07-19 10:43:39 +0530

    Another update

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -8,19 +8,18 @@ function HeroSection() {
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-7 gap-12 items-center px-6">
         
         {/* IMAGE MOBILE */}
-        <div className="md:hidden mb-1 w-full h-64 border border-gray-500 rounded-xl flex items-center justify-center bg-gray-800 overflow-hidden">
+        <div className="md:hidden mb-1 w-full h-64 relative border border-gray-500 rounded-xl bg-gray-800 overflow-hidden">
           <Image
             src="/hero.jpg"
             alt="Hero illustration"
             fill
-            style={{ objectFit: 'cover' }}
-            className="rounded-xl"
-            sizes="(max-width: 768px) 100vw, 100vw"
+            className="object-cover rounded-xl"
+            sizes="(max-width: 768px) 100vw"
             priority
           />
         </div>
 
-        {/* TEXT */}
+        {/* TEXT SECTION */}
         <div className="md:col-span-3 space-y-4 mx-3 flex flex-col items-center md:items-start text-left">
           <h2 className="text-3xl md:text-4xl font-bold max-w-md">
             Get There Faster — With Someone Who Just Did
@@ -33,21 +32,20 @@ function HeroSection() {
           </p>
           <Link
             href="/mentors"
-            className="block mx-auto md:mx-0 w-44 md:w-auto text-center mt-5 bg-white  text-black px-6 py-3 rounded-md text-xl sm:text-lg hover:bg-gray-100 transform hover:scale-105 transition duration-300 shadow-md"
+            className="block mx-auto md:mx-0 w-44 md:w-auto text-center mt-5 bg-white text-black px-6 py-3 rounded-md text-xl sm:text-lg hover:bg-gray-100 transform hover:scale-105 transition duration-300 shadow-md"
           >
             Book Now
           </Link>
         </div>
 
         {/* IMAGE DESKTOP */}
-        <div className="hidden md:flex md:col-span-4 w-full h-96 border border-gray-500 rounded-xl items-center justify-center bg-gray-800 overflow-hidden relative">
+        <div className="hidden md:block md:col-span-4 w-full h-96 relative border border-gray-500 rounded-xl bg-gray-800 overflow-hidden">
           <Image
             src="/hero.jpg"
             alt="Hero illustration"
             fill
-            style={{ objectFit: 'cover' }}
-            className="rounded-xl"
-            sizes="(min-width: 768px) 100vw, 100vw"
+            className="object-cover rounded-xl"
+            sizes="(min-width: 768px) 100vw"
             priority
           />
         </div>
